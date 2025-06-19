c503c78f90ae60740a506cc0917bb33cb97b217c
Kenchik <Kenchik@DESKTOP-PU2T3FD>
2025-06-19 17:34:28 +0500

    fdsfsd

---
+++ WpfApp36/AddToDoWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+
+namespace WpfApp36
+{
+    public partial class AddToDoWindow : Window
+    {
+        public string ToDoTitle => titleBox.Text;
+        public DateTime? ToDoDate => datePicker.SelectedDate;
+        public string ToDoDescription => descBox.Text;
+
+        public AddToDoWindow()
+        {
+            InitializeComponent();
+            datePicker.SelectedDate = DateTime.Now;
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(ToDoTitle) || !ToDoDate.HasValue)
+            {
+                MessageBox.Show("Заполните все поля!");
+                return;
+            }
+            DialogResult = true;
+            Close();
+        }
+    }
+}
---
+++ WpfApp36/TomorrowConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WpfApp36
+{
+    public class TomorrowConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime date)
+                return date.Date == DateTime.Now.Date.AddDays(1);
+            return false;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
